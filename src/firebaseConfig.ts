5dd5042b044867bbf8d8111f71ededf3863648f6
Joseph Brennan <josephb@wolfram.com>
2026-02-02 22:06:43 +0000

    include config file

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyAE33P92IEoZU38JzRzp5krK3bzazq8R-g',
+	authDomain: 'perfect-scrabble-games.firebaseapp.com',
+	projectId: 'perfect-scrabble-games',
+	storageBucket: 'perfect-scrabble-games.firebasestorage.app',
+	messagingSenderId: '993938539832',
+	appId: '1:993938539832:web:3c0a96929280948a36edcd',
+	measurementId: 'G-T63042C8X8',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export { db, analytics };
