899f5ccedd31761ff9111ee03b52528e78ddf308
Joseph Brennan <josephb@wolfram.com>
2026-02-07 13:55:18 +0000

    enable anonymous users

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,5 +19,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db, analytics };
+// Enable anonymous authentication
+signInAnonymously(auth).catch((error) => {
+	console.error('Failed to authenticate anonymously:', error);
+});
+
+export { db, analytics, auth };
